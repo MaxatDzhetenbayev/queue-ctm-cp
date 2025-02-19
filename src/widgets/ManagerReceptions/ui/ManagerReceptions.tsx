447e9118ddb3ed0b93145ed2e8a8f937808587c4
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-02-19 11:23:16 +0500

    update: Лоадинг скелетон

--- src/widgets/ManagerReceptions/ui/ManagerReceptions.tsx
+++ src/widgets/ManagerReceptions/ui/ManagerReceptions.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React from "react";
 import { useManagerReceptions } from "../hooks/index";
 import { receptionTransformDto } from "../../../entities/receptions/helpers";
@@ -8,6 +7,7 @@ import {
   ReceptionDetail,
   Statuses,
 } from "@/features";
+import { Box, Skeleton, Table, Title } from "@mantine/core";
 
 export const ManagerReceptions = () => {
   const { data, isLoading, isError } = useManagerReceptions();
@@ -24,47 +24,60 @@ export const ManagerReceptions = () => {
   ];
 
   return (
-    <div className="overflow-auto h-full max-h-[330px]">
+    <Box component="section" className="overflow-auto h-full max-h-[330px]">
       {isLoading ? (
-        <div>loading...</div>
+        <Box>
+          <Skeleton h={35} w={150} />
+          <Skeleton mt={20} h={35} w="100%" />
+          <Skeleton mt={5} h={35} w="100%" />
+          <Skeleton mt={5} h={35} w="100%" />
+          <Skeleton mt={5} h={35} w="100%" />
+          <Skeleton mt={5} h={35} w="100%" />
+          <Skeleton mt={5} h={35} w="100%" />
+          <Skeleton mt={5} h={35} w="100%" />
+        </Box>
       ) : (
         <>
-          <h2 className="text-2xl font-semibold mb-4">Приемы</h2>
+          <Title mb={20} order={2}>
+            Приемы
+          </Title>
           {data === undefined ? (
             <div>Нет записей</div>
           ) : (
-            <table className="w-full">
-              <thead className="">
-                <tr className="bg-neutral-50">
+            <Table>
+              <Table.Thead>
+                <Table.Tr>
                   {receptionTableHeader.map((header) => (
-                    <th
+                    <Table.Th
                       key={header}
                       className="px-6 py-3 text-left text-sm font-medium"
                     >
                       {header}
-                    </th>
+                    </Table.Th>
                   ))}
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-neutral-200">
+                </Table.Tr>
+              </Table.Thead>
+              <Table.Tbody className="divide-y divide-neutral-200">
                 {receptionTransformDto(data).map((reception) => (
-                  <tr
+                  <Table.Tr
                     key={reception.id}
                     className="hover:bg-neutral-50 transition-colors"
                   >
-                    <td className="px-6 py-2 text-sm">
+                    <Table.Td className="px-6 py-2 text-sm">
                       {reception.profile.full_name}
-                    </td>
-                    <td className="px-6 py-2 text-sm">
+                    </Table.Td>
+                    <Table.Td className="px-6 py-2 text-sm">
                       {reception.profile.phone}
-                    </td>
-                    <td className="px-6 py-2 text-sm">{reception.time}</td>
-                    <td className="px-6 py-2">
+                    </Table.Td>
+                    <Table.Td className="px-6 py-2 text-sm">
+                      {reception.time}
+                    </Table.Td>
+                    <Table.Td className="px-6 py-2">
                       <span className="px-2 py-1 text-xs rounded-full bg-primary-100">
                         {reception.status.name}
                       </span>
-                    </td>
-                    <td className="px-6 py-2 space-x-2">
+                    </Table.Td>
+                    <Table.Td className="px-6 py-2 space-x-2">
                       {reception.status.id === Statuses.DONE && (
                         <ReceptionDetail id={reception.id} />
                       )}
@@ -95,14 +108,14 @@ export const ManagerReceptions = () => {
                           Завершить
                         </ChangeReceptiontionStatusButton>
                       )}
-                    </td>
-                  </tr>
+                    </Table.Td>
+                  </Table.Tr>
                 ))}
-              </tbody>
-            </table>
+              </Table.Tbody>
+            </Table>
           )}
         </>
       )}
-    </div>
+    </Box>
   );
 };
