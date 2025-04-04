9b8a73e88f4cdd4138d961d3e29dc4d924e856e2
Dzhetenbaev Maksat <maxat.dzhetenbaev@gmail.com>
2025-04-04 13:24:33 +0500

    feat: Автозаполнение полей по иин для записи оффлайн клиента

--- src/features/reception-create-offline/ui/ReceptionCreateOffline.tsx
+++ src/features/reception-create-offline/ui/ReceptionCreateOffline.tsx
@@ -2,10 +2,10 @@
 import { api } from '@/shared';
 import { queryClient } from '@/shared/providers/query-providers';
 import { Button, Flex, Input, Modal, Select, Title } from '@mantine/core'
-import { useDisclosure } from '@mantine/hooks';
+import { useDebouncedValue, useDisclosure } from '@mantine/hooks';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Controller, useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 
@@ -15,10 +15,8 @@ interface FormData {
     iin: string;
     phone: string,
     visitor_type_id: string,
-    service_id: number;
+    service_id: number | null;
 }
-
-
 
 export const ReceptionCreateOffline = () => {
 
@@ -33,7 +31,35 @@ export const ReceptionCreateOffline = () => {
         }
     })
 
-    const { control, handleSubmit, reset } = useForm<FormData>();
+    const { control, handleSubmit, reset, watch } = useForm<FormData>();
+
+    const iin = watch('iin')
+    const [debouncedIin] = useDebouncedValue(iin, 1000);
+
+
+    const { data: userByIin, isSuccess: isUserFindSuccess, isLoading: isUserFindLoading, } = useQuery({
+        queryKey: ['finded-user-by-iin', debouncedIin],
+        queryFn: async () => {
+            const res = await api.get('/users/iin/' + iin)
+            return res.data
+        },
+        enabled: !!debouncedIin,
+        retry: false,
+        refetchOnWindowFocus: false,
+    })
+
+    useEffect(() => {
+        if (isUserFindSuccess) {
+            reset((prev) => ({
+                ...prev,
+                full_name: userByIin.full_name,
+                phone: userByIin.phone,
+                visitor_type_id: String(userByIin.visitor_type_id),
+                service_id: userByIin.service_id
+            }))
+        }
+    }, [userByIin, isUserFindSuccess, reset])
+
 
     const { mutate } = useMutation({
         mutationKey: ["receptions-create-offline"],
@@ -66,58 +92,89 @@ export const ReceptionCreateOffline = () => {
                 <form onSubmit={handleSubmit(onSubmit)} >
                     <Flex direction="column" gap={15} mt={20}>
                         <Controller
-                            name="full_name"
-                            control={control}
-                            render={({ field }) => <Input placeholder="Введите полное ФИО"  {...field} />}
-                        />
-                        <Controller
                             name="iin"
                             control={control}
-                            render={({ field }) => <Input placeholder="Введите ИИН"  {...field} />}
+                            render={({ field }) => <Input maxLength={12} placeholder="Введите ИИН"  {...field} />}
                         />
-                        <Controller
-                            name="phone"
-                            control={control}
-                            render={({ field }) => <Input placeholder="Введите номер телефона"  {...field} />}
-                        />
-                        <Controller
-                            name="visitor_type_id"
-                            control={control}
-                            render={({ field }) => (
-                                <Select
-                                    value={String(field.value)}
-                                    onChange={(value) => field.onChange(Number(value))}
-                                    placeholder="Выберите тип посетителя"
-                                    defaultValue="2"
-                                    data={[
-                                        {
-                                            value: "1",
-                                            label: "Работодатель"
-                                        },
-                                        {
-                                            value: "2",
-                                            label: "Соискатель"
-                                        },
-                                    ]}
-                                />
-                            )} />
-                        <Controller
-                            name="service_id"
-                            control={control}
-                            render={({ field }) => (
-                                <Select
-                                    value={String(field.value)}
-                                    onChange={(value) => field.onChange(Number(value))}
-                                    placeholder="Выберите сервис"
-                                    data={!isServicesLoading && managerServices.map((s: { id: number, name: { [key: string]: string } }) => (
-                                        {
-                                            value: String(s.id),
-                                            label: s.name["ru"]
-                                        }
-                                    ))
-                                        || []}
-                                />
-                            )} />
+                        {!!debouncedIin && !isUserFindLoading && (
+                            <>
+                                {isUserFindSuccess ? (
+                                    <>
+                                        <Input disabled value={userByIin.full_name} placeholder="Введите полное ФИО" />
+                                        <Input disabled value={userByIin.phone} placeholder="Введите номер телефона" />
+                                        <Select
+                                            disabled
+                                            value={userByIin.visitor_type_id}
+                                            placeholder="Тип посетителя"
+                                            data={[
+                                                { value: "1", label: "Работодатель" },
+                                                { value: "2", label: "Соискатель" },
+                                            ]}
+                                        />
+                                        <Controller
+                                            name="service_id"
+                                            control={control}
+                                            render={({ field }) => (
+                                                <Select
+                                                    value={String(field.value)}
+                                                    onChange={(value) => field.onChange(Number(value))}
+                                                    placeholder="Выберите сервис"
+                                                    data={!isServicesLoading && managerServices.map((s: { id: number, name: { [key: string]: string } }) => ({
+                                                        value: String(s.id),
+                                                        label: s.name["ru"]
+                                                    })) || []}
+                                                />
+                                            )}
+                                        />
+                                    </>
+                                ) : (
+                                    <>
+                                        <Controller
+                                            name="full_name"
+                                            control={control}
+                                            render={({ field }) => <Input placeholder="Введите полное ФИО"  {...field} />}
+                                        />
+                                        <Controller
+                                            name="phone"
+                                            control={control}
+                                            render={({ field }) => <Input maxLength={11} placeholder="Введите номер телефона"  {...field} />}
+                                        />
+                                        <Controller
+                                            name="visitor_type_id"
+                                            control={control}
+                                            render={({ field }) => (
+                                                <Select
+                                                    value={String(field.value)}
+                                                    onChange={(value) => field.onChange(Number(value))}
+                                                    placeholder="Выберите тип посетителя"
+                                                    defaultValue="2"
+                                                    data={[
+                                                        { value: "1", label: "Работодатель" },
+                                                        { value: "2", label: "Соискатель" },
+                                                    ]}
+                                                />
+                                            )}
+                                        />
+                                        <Controller
+                                            name="service_id"
+                                            control={control}
+                                            render={({ field }) => (
+                                                <Select
+                                                    value={String(field.value)}
+                                                    onChange={(value) => field.onChange(Number(value))}
+                                                    placeholder="Выберите сервис"
+                                                    data={!isServicesLoading && managerServices.map((s: { id: number, name: { [key: string]: string } }) => ({
+                                                        value: String(s.id),
+                                                        label: s.name["ru"]
+                                                    })) || []}
+                                                />
+                                            )}
+                                        />
+                                    </>
+                                )}
+                            </>
+                        )}
+
                         <Button type='submit' bg="dark">Создать запись</Button>
                     </Flex>
                 </form>
