3e8d613948d19515af3ae5a63d7f383eb1fb7092
Dzhetenbaev Maksat <maxat.dzhetenbaev@gmail.com>
2025-05-19 17:02:20 +0500

    change: Временно отключил проверку middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,23 +10,23 @@ interface DecodedToken {
 }
 
 export function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-  const token = req.cookies.get("access_token");
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-  try {
-    const decoded = jwt.decode(token.value) as DecodedToken;
-    if (decoded.role === "manager" && !pathname.startsWith("/dashboard")) {
-      return NextResponse.redirect(new URL("/dashboard", req.url));
-    }
-    if (decoded.role === "admin" && !pathname.startsWith("/admin")) {
-      return NextResponse.redirect(new URL("/admin", req.url));
-    }
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (error) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+  // const { pathname } = req.nextUrl;
+  // const token = req.cookies.get("access_token");
+  // if (!token) {
+  //   return NextResponse.redirect(new URL("/login", req.url));
+  // }
+  // try {
+  //   const decoded = jwt.decode(token.value) as DecodedToken;
+  //   if (decoded.role === "manager" && !pathname.startsWith("/dashboard")) {
+  //     return NextResponse.redirect(new URL("/dashboard", req.url));
+  //   }
+  //   if (decoded.role === "admin" && !pathname.startsWith("/admin")) {
+  //     return NextResponse.redirect(new URL("/admin", req.url));
+  //   }
+  // // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // } catch (error) {
+  //   return NextResponse.redirect(new URL("/login", req.url));
+  // }
 }
 
 export const config = {
