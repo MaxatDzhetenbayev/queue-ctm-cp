3a5c97d61e835812050e74d1b205e056ec783f51
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-06-23 11:06:26 +0500

    upd: Отправка трансформированного времени

--- src/entities/receptions/helpers/index.ts
+++ src/entities/receptions/helpers/index.ts
@@ -6,6 +6,10 @@ export const receptionTransformDto = (
 ): IReceptionFormated[] => {
   return data.map((item) => ({
     ...item,
+    time: new Date(item.time).toLocaleTimeString("ru-RU", {
+      hour: "2-digit",
+      minute: "2-digit",
+    }),
     profile: item.user.profile,
     user: { id: item.user.id },
   }));
