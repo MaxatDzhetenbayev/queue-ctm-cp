ec01d0e4884d6ea3fb637c9debc39d8c736d4bbd
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-06-23 12:42:33 +0500

    fix: Исправлены новые поля и убраны ненужные

--- src/features/reception-detail/ui/ReceptionDetail.tsx
+++ src/features/reception-detail/ui/ReceptionDetail.tsx
@@ -16,26 +16,29 @@ export const ReceptionDetail = ({ id }: { id: number }) => {
         ) : (
           <section>
             <h1 className="font-bold text-2xl">
-              Имя: {data?.user?.profile?.full_name}
+              Имя: {data?.user?.profile?.fullName}
             </h1>
             <section className="mt-4">
               <p>
-                <strong>ИИН:</strong>{data?.user?.profile?.iin}
+                <strong>ИИН:</strong>
+                {data?.user?.profile?.iin}
               </p>
               <p>
                 <strong>Телефон:</strong> {data?.user?.profile?.phone}
               </p>
               <p>
-                <strong>Дата записи:</strong> {data?.date}
+                <strong>Дата записи:</strong>{" "}
+                {new Date(data?.date as string).toLocaleDateString("ru-RU")}
               </p>
               <p>
-                <strong>Время записи:</strong> {data?.time}
+                <strong>Время записи:</strong>{" "}
+                {new Date(data?.time as string).toLocaleTimeString("ru-RU", {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
               </p>
               <p>
-                <strong>Статус:</strong> {normalizeStatus(data?.status?.name)}
-              </p>
-              <p>
-                <strong>Тип посетителя:</strong> {data?.user?.visitor_type?.name === "employer" ? "Работодатель" : "Соискатель"}
+                <strong>Статус:</strong> {normalizeStatus(data?.status)}
               </p>
               <p>
                 <strong>Выбранный сервис:</strong> {data?.service?.name?.["ru"]}
