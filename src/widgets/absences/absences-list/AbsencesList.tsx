cce319f8e48681ca8865e9a157dfefd83b1ac74d
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-07-08 15:38:34 +0500

    upd: страница отсутствия - список отсутствии - стили для высоты

--- src/widgets/absences/absences-list/AbsencesList.tsx
+++ src/widgets/absences/absences-list/AbsencesList.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import {
+  Box,
   Button,
   Flex,
   Modal,
@@ -73,33 +74,35 @@ export const AbsencesList = () => {
   });
 
   return (
-    <Paper withBorder p={20}>
+    <Paper withBorder p={20} h={500} style={{ position: "relative" }}>
       <Flex justify="space-between" align="center">
         <Title order={3}>Список отсутствий</Title>
         <CreateAbsenceModal />
       </Flex>
-      <Table mt={20} stickyHeader stickyHeaderOffset={60}>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>Сотрудник</Table.Th>
-            <Table.Th>Тип</Table.Th>
-            <Table.Th>Период</Table.Th>
-            <Table.Th>Действия</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {isLoading ? (
-            <></>
-          ) : (
-            <>
-              {data?.data?.map((item) => (
-                <AbsencesItem key={item.id} {...item} />
-              ))}
-            </>
-          )}
-        </Table.Tbody>
-      </Table>
-      <Flex justify="space-between" align={"center"} mt={10}>
+      <Flex h={420} direction={"column"}>
+        <Box flex={1}>
+          <Table mah={420} stickyHeader stickyHeaderOffset={60}>
+            <Table.Thead>
+              <Table.Tr>
+                <Table.Th>Сотрудник</Table.Th>
+                <Table.Th>Тип</Table.Th>
+                <Table.Th>Период</Table.Th>
+                <Table.Th>Действия</Table.Th>
+              </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>
+              {isLoading ? (
+                <></>
+              ) : (
+                <>
+                  {data?.data?.map((item) => (
+                    <AbsencesItem key={item.id} {...item} />
+                  ))}
+                </>
+              )}
+            </Table.Tbody>
+          </Table>
+        </Box>
         <Pagination
           total={Math.ceil((data?.total || 0) / limit)}
           value={page}
