4ec4feefecd77ecc3645f6c4cc19344da6b74a9a
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-07-18 11:19:04 +0500

    upd: страница not-found - кнопка вернуться назад

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,20 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function NotFound() {
+  const router = useRouter();
+
   return (
     <div className="flex h-screen items-center justify-center flex-col">
       <h1 className="text-4xl font-bold">404 - Страница не найдена</h1>
       <p className="text-gray-600 mt-2">Такой страницы не существует.</p>
+      <button
+        className="text-blue-500 mt-4 hover:underline"
+        onClick={() => router.back()}
+      >
+        Вернуться назад
+      </button>
     </div>
   );
 }
