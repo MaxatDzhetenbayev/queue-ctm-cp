1b3be7cf8d01bfef1de34db91b389e9bf6a041e3
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-07-20 19:00:48 +0500

    config: client - добавление axios клиента

--- src/shared/lib/client/useCustomMutation.ts
+++ src/shared/lib/client/useCustomMutation.ts
@@ -1,7 +1,7 @@
 import { AxiosError } from "axios";
 import { toast } from "react-toastify";
 
-import { queryClient } from "./config";
+import { queryClient } from "./query-config";
 
 import { useMutation, UseMutationOptions } from "@tanstack/react-query";
 
