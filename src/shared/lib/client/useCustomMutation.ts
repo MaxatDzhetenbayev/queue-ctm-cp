0a503ab5809063ec6228e520fd91e43d5d664f48
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-07-20 18:58:22 +0500

    config: client - добавление кастомного use-mutation

---
+++ src/shared/lib/client/useCustomMutation.ts
@@ -0,0 +1,92 @@
+import { AxiosError } from "axios";
+import { toast } from "react-toastify";
+
+import { queryClient } from "./config";
+
+import { useMutation, UseMutationOptions } from "@tanstack/react-query";
+
+/**
+ * Кастомный хук для выполнения мутаций с использованием React Query и Axios.
+ * Предварительно обрабатывает успех и ошибки, отображая уведомления с помощью toast.
+ *
+ * @param {Object} mutationFn - Объект с параметрами для хука.
+ * @param {function(TVariables): Promise<TData>} mutationFn.mutationFn - Функция, выполняющая мутацию и возвращающая промис.
+ * @param {UseMutationOptions<TData, TError, TVariables, TContext>} [mutationFn.mutationConfig] - Дополнительные параметры конфигурации для мутации.
+ * @param {Object} [mutationFn.customConfig] - Дополнительные настройки для кастомизации поведения мутации.
+ * @param {Array<Array<unknown>>} [mutationFn.customConfig.invalidateQueries] - Массив ключей запросов, которые нужно инвалидировать после успешной мутации.
+ * @param {Object} [mutationFn.toastConfig] - Конфигурация для уведомлений toast, содержащая сообщения для успеха и ошибки.
+ * @param {string|function(TData): string} [mutationFn.toastConfig.successMessage] - Сообщение для успешной мутации.
+ * @param {string|function(TError): string} [mutationFn.toastConfig.errorMessage] - Сообщение для ошибки мутации.
+ * @returns Объект мутации, содержащий методы и состояние, связанные с мутацией.
+ */
+export function useCustomMutation<
+  TData = unknown,
+  TError = AxiosError,
+  TVariables = unknown,
+  TContext = unknown
+>({
+  mutationFn,
+  mutationConfig = {},
+  customConfig = {},
+  toastConfig = {},
+}: {
+  mutationFn: (variables: TVariables) => Promise<TData>;
+  mutationConfig?: UseMutationOptions<TData, TError, TVariables, TContext>;
+  customConfig?: {
+    invalidateQueries?: Array<Array<unknown>>;
+  };
+  toastConfig?: {
+    successMessage?: string | ((data: TData) => string);
+    errorMessage?: string | ((error: TError) => string);
+  };
+}) {
+  const mutation = useMutation({
+    mutationFn: mutationFn,
+    onSuccess: (data, variables, context) => {
+      if (customConfig?.invalidateQueries) {
+        customConfig.invalidateQueries.forEach((queryKey) => {
+          queryClient.invalidateQueries({ queryKey: [queryKey] });
+        });
+      }
+
+      if (toastConfig?.successMessage) {
+        const successMessage =
+          typeof toastConfig?.successMessage === "function"
+            ? toastConfig.successMessage(data)
+            : toastConfig?.successMessage;
+
+        toast.success(successMessage);
+      } else {
+        toast.success("Успешная мутация!");
+      }
+
+      mutationConfig?.onSuccess?.(data, variables, context);
+    },
+    onError: (error, variables, context) => {
+      const axiosError = error as AxiosError;
+
+      const rawMessage = (
+        axiosError.response?.data as { message?: string | string[] } | undefined
+      )?.message;
+
+      const errorMessage =
+        typeof toastConfig?.errorMessage === "function"
+          ? toastConfig.errorMessage(error)
+          : toastConfig?.errorMessage;
+
+      if (errorMessage) {
+        toast.error(errorMessage);
+      } else if (Array.isArray(rawMessage)) {
+        rawMessage.forEach((msg) => toast.error(msg));
+      } else if (typeof rawMessage === "string") {
+        toast.error(rawMessage);
+      } else {
+        toast.error(axiosError.message ?? "Произошла ошибка");
+      }
+
+      mutationConfig?.onError?.(error, variables, context);
+    },
+    ...mutationConfig,
+  });
+  return mutation;
+}
