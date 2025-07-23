5f93225ea42f07501ff3bf992b9080994e1a6457
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-07-23 17:47:33 +0500

    upd: менеджеры - проверка статуса работы - обработка ошибки 400 на конец времени и убрана кнопка закрытия у модального окна

--- src/features/manager-start-work/ManagerStartWork.tsx
+++ src/features/manager-start-work/ManagerStartWork.tsx
@@ -4,11 +4,11 @@ import { api } from "@/shared";
 import { Button, Modal, Stack, Title } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { useMutation, useQuery } from "@tanstack/react-query";
-
+import "./ManagerStartWork.css";
 export const ManagerStartWork = () => {
   const [opened, { close }] = useDisclosure(true);
 
-  const { data, isLoading } = useQuery<{
+  const { data, isLoading, isError } = useQuery<{
     isOnline: boolean;
   }>({
     queryKey: ["manager-online-check"],
@@ -16,6 +16,7 @@ export const ManagerStartWork = () => {
       const res = await api.get("/users/managers/online-check");
       return res.data;
     },
+    retry: false,
   });
 
   const { mutate: startWork } = useMutation({
@@ -33,12 +34,20 @@ export const ManagerStartWork = () => {
   return (
     <Modal onClose={() => {}} opened={opened} centered size="lg">
       <Stack justify="center" align="center" style={{ height: "100%" }}>
-        <Title order={3} mb="md">
-          Добро пожаловать в панель управления очередью!
+        <Title order={3} mb="md" style={{ textAlign: "center" }}>
+          {isError ? (
+            <>
+              Рабочее время окончено. <br /> До следующего рабочего дня!
+            </>
+          ) : (
+            "Добро пожаловать в панель управления очередью!"
+          )}
         </Title>
-        <Button onClick={() => startWork()} color="dark">
-          Приступить к работе
-        </Button>
+        {!isError && (
+          <Button onClick={() => startWork()} color="dark" disabled={isError}>
+            Приступить к работе
+          </Button>
+        )}
       </Stack>
     </Modal>
   );
