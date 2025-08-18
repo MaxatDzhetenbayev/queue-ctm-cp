3eb47b4ab9125147b4b1afe46316a348821a027b
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-08-18 15:53:03 +0500

    ref: пользователи - персонал - поиск - смена инпута на shadcn

--- src/modules/users/ui/employee/EmployeeQuery.tsx
+++ src/modules/users/ui/employee/EmployeeQuery.tsx
@@ -1,17 +1,21 @@
-import { Search } from 'lucide-react';
+import { Search } from "lucide-react";
 import React from "react";
 
+import { Input } from "@/shared/components/ui/input";
 
 interface EmployeeQueryProps {
   inputValue: string;
   setInputValue: (value: string) => void;
 }
 
-export const EmployeeQuery = ({ inputValue, setInputValue }: EmployeeQueryProps) => {
+export const EmployeeQuery = ({
+  inputValue,
+  setInputValue,
+}: EmployeeQueryProps) => {
   return (
     <div className="flex-1 relative">
       <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
-      <input
+      <Input
         type="text"
         placeholder="Поиск по ФИО сотрудника..."
         className="w-full pl-10 pr-4 py-2.5 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
