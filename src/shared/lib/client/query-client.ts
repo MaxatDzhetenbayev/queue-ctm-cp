9d077fe856264b4c828efbb442c331f117e2f533
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-08-29 17:25:11 +0500

    upd: отключение рефетча на фокус страницы

--- src/shared/lib/client/query-client.ts
+++ src/shared/lib/client/query-client.ts
@@ -3,7 +3,7 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: true,
+      refetchOnWindowFocus: false,
       refetchOnReconnect: true,
       refetchOnMount: false,
       retry: 3,
